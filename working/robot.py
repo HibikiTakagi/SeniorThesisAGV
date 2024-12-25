6e57cc138b90e249a7a4ee249f51d492b6ae5e8c
Hibiki Takagi <takagihbk@titanium.acsl.ics.keio.ac.jp>
2024-12-25 19:52:59 +0900

    タスクキューをリストに変名、ランダムなロボットリストの生成

--- working/robot.py
+++ working/robot.py
@@ -1,11 +1,11 @@
 import numpy as np
 from numpy.typing import NDArray
-from pack import Pack
+import random
 
 class Robot:
   def __init__(self, r_id, current_node):
     self.id = r_id
-    self.pack_queue = []
+    self.pack_list = []
     self.start_node = current_node
     self.current_node = current_node
     self.pack = None
@@ -14,30 +14,46 @@ class Robot:
     self.current_task = None
     
   def __repr__(self):
-    return f'Robot(id:{self.id}, pack_queue:{self.pack_queue}, current_node:{self.current_node})'
+    return f'Robot(id:{self.id}, pack_list:{self.pack_list}, current_node:{self.current_node})'
     
   @property
   def name(self):
     return f"R{self.id}"
+
+  @property
+  def pack_id_queue(self):
+    return [pack.id for pack in self.pack_list]  
+  
+  def reset(self):
+    self.pack_list = []
+    self.current_node = self.start_node
+    self.pack = None
+    self.current_pack = None
+    self.current_task = None
     
-  def queue_time_from_start(self, graph:NDArray[np.int32], pack_list:list[Pack]):
+  def queue_time_from_start(self, graph:NDArray[np.int32]):
     end_time = 0
     current_node = self.start_node
         
-    for pack_id in self.pack_queue:
-      pack = pack_list[pack_id]
+    for pack in self.pack_list:
       destination = pack.start_node
             
       move_task_durations = graph[current_node, destination]
       end_time += move_task_durations
-      
+            
       current_node = pack.start_node
       destination = pack.target_node
-
+            
       task_durations = graph[current_node, destination]
       end_time += task_durations
             
     return end_time
             
+def generate_robot_list_random(number:int, len_node:int)->list[Robot]:
+  assert number <= len_node
     
-        
+  current_nodes = random.sample(range(len_node), number)
+  robot_list = []
+  for r_id in range(number):
+    robot_list.append(Robot(r_id=r_id, current_node=current_nodes[r_id]))
+  return robot_list
