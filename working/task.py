1bbc4df1af17024d2d5571af846c8b3126354d26
Hibiki Takagi <takagihbk@titanium.acsl.ics.keio.ac.jp>
2024-12-25 18:41:15 +0900

    path_distanceの更新とランダムなタスクリストの生成

--- working/task.py
+++ working/task.py
@@ -1,6 +1,7 @@
 import numpy as np
+import random
 from numpy.typing import NDArray
-from map_maker import NODE_DISTANCES
+from test_map import TEST_NODE_DISTANCES
 
 class Task:
   def __init__(self, t_id:int, start_node:int, target_node:int, birth_time:int, graph:NDArray[np.int32]):
@@ -23,14 +24,27 @@ class Task:
     
   def complete(self):
     self.is_complete = True
+  
+  def update_path_distance(self, graph:NDArray[np.int32]):
+    self.path_distance = graph[self.start_node, self.target_node]
 
 class DummyTask(Task):
   def __init__(self, start_node, target_node, birth_time, graph):
     super().__init__(-1, start_node, start_node, birth_time, graph)
     self.is_complete = True
 
+def generate_task_list_random(number:int, map_len_node:int, graph:NDArray[np.int32])->list[Task]:
+  task_list = []
+  for t_id in range(number):
+    pair = random.sample(range(map_len_node), 2)
+    start_node = pair[0]
+    target_node = pair[1]
+    birth_time = 0
+    task_list.append(Task(t_id, start_node, target_node, birth_time, graph))
+  return task_list
+
 if __name__ == "__main__":
-  task_a = Task(0, 0, 1, 0, NODE_DISTANCES)
+  task_a = Task(0, 0, 1, 0, TEST_NODE_DISTANCES)
   print(task_a)
   print(task_a.get_waiting_time(10))
   task_a.complete()
