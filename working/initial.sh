35571e55827bac4a7aa6398bd0e39057c0b639ae
Hibiki Takagi <takagihbk@titanium.acsl.ics.keio.ac.jp>
2024-12-28 17:53:53 +0900

    line_profilerのインストール

--- working/initial.sh
+++ working/initial.sh
@@ -1,2 +1,3 @@
 python3.10 -m pip install "numpy==1.23.5"
 python3.10 -m pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.0.1+cu118.html
+python3.10 -m pip install line_profiler
