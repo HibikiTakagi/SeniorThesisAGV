7eb248fcbcd7d3266d5ab04738cf8b530b34be36
Hibiki Takagi <takagihbk@titanium.acsl.ics.keio.ac.jp>
2024-12-30 18:21:45 +0900

    torchinfoのインストール

--- working/initial.sh
+++ working/initial.sh
@@ -2,3 +2,4 @@ python3.10 -m pip install "numpy==1.23.5"
 python3.10 -m pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.0.1+cu118.html
 python3.10 -m pip install line_profiler
 python3.10 -m pip install plotly
+python3.10 -m pip install torchinfo
