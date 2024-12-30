75eb08ad091fd36858b6cb687c8fc0835b2a9ac6
Hibiki Takagi <takagihbk@titanium.acsl.ics.keio.ac.jp>
2024-12-30 13:42:01 +0900

    報酬や損失のグラフへの描写

--- working/evaluation.py
+++ working/evaluation.py
@@ -1,9 +1,12 @@
 import copy
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 from env import JobSchedulerEnv
 from distribute_policy import DistributePolicy, DQN_DistributePolicy
 from robot import Robot
 from task import Task
 from scheduler import Scheduler
+from utils_save_fig import save_fig_reward, save_fig_gantt_resource, save_fig_gantt_task
 from SETTING import MODEL_PATH, NODE_DISTANCES
 
 class Evaluation:
@@ -17,12 +20,36 @@ class Evaluation:
       env.scheduler.distribute_policy.load_model(MODEL_PATH)
     env.scheduler.distribute()
     resources, tasks, resource_allocation, start_times, durations, end_times = env.scheduler.dump_schedules(NODE_DISTANCES)
-    print(resources)
-    print(tasks)
-    print(resource_allocation)
-    print(start_times)
-    print(durations)
-    print(end_times)
+    save_fig_gantt_resource(tasks, resource_allocation, start_times, end_times)
+    save_fig_gantt_task(tasks, start_times, end_times)
+    return copy.deepcopy((resources, tasks, resource_allocation, start_times, durations, end_times))
+
+class StepEvaluation:
+  def __init__(self):
+    pass
+
+  def main(self):
+    env = JobSchedulerEnv()
+    assert isinstance(env.scheduler.distribute_policy, DQN_DistributePolicy)
+    env.scheduler.distribute_policy.load_model(MODEL_PATH)
+    env.scheduler.distribute_policy.eval()
+    ts = 0
+    state, info = env.reset()
+    terminated, truncated = (False, False)
+    rewards = []
+
+    while not (terminated or truncated):
+      ts += 1
+      action = env.scheduler.distribute_policy.act(*state)
+      next_state, reward, terminated, truncated, info = env.step(action)
+      state = next_state
+      rewards.append(reward)
+
+    save_fig_reward(rewards)
+    resources, tasks, resource_allocation, start_times, durations, end_times = env.scheduler.dump_schedules(NODE_DISTANCES)
+
+    save_fig_gantt_resource(tasks, resource_allocation, start_times, end_times)
+    save_fig_gantt_task(tasks, start_times, end_times)
     return copy.deepcopy((resources, tasks, resource_allocation, start_times, durations, end_times))
 
 if __name__ == "__main__":
--- working/learning.py
+++ working/learning.py
@@ -1,8 +1,13 @@
+import copy
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 from env import JobSchedulerEnv
 from distribute_policy import DQN_DistributePolicy
-from SETTING import MODEL_PATH
+from utils_save_fig import save_fig_losses, save_fig_return, save_fig_reward
+from SETTING import MODEL_PATH, NODE_DISTANCES
 
-EPISODE = 1000
+EPISODE = 400
+#EPISODE = 2
 SYNC = 10
 TRAIN_FREQ = 10
 
@@ -17,34 +22,43 @@ class Learning:
     gamma = env.scheduler.distribute_policy.gamma
     ret = 0
     ts = 0
+    losses = []
+    rets = []
     
-    for eps in range(EPISODE):
+    for eps in tqdm(range(EPISODE)):
       state, info = env.reset()
       terminated, truncated = (False, False)
-      #for time in range(): # 以下のwhileで事足りそう
+      #for time in range(): # 下記のwhileで事足りそう
+      rewards = []
+      
       while not (terminated or truncated):
-        ts += 0
+        ts += 1
         action = env.scheduler.distribute_policy.act(*state)
         next_state, reward, terminated, truncated, info = env.step(action)
         env.scheduler.distribute_policy.remember(state, action, reward, next_state, terminated, truncated, info)
         state = next_state
         
         ret = reward + gamma*ret
-        if terminated or truncated:
-          if env.scheduler.distribute_policy.loss is None:
-            print(f"Episode {eps+1}/{EPISODE} - Final reward: {reward:5.3f}, Return:{ret:5.3f} Epsilon: {env.scheduler.distribute_policy.epsilon:.2f}")
-          else:
-            print(f"Episode {eps+1}/{EPISODE} - Final reward: {reward:5.3f}, Return:{ret:5.3f} Epsilon: {env.scheduler.distribute_policy.epsilon:.2f}, loss:{env.scheduler.distribute_policy.loss:.3f}")
-      
-      if ts % TRAIN_FREQ == 0:
-        ts = 0   
-        env.scheduler.distribute_policy.train()
+        rewards.append(reward)
+        
+        if ts % TRAIN_FREQ == 0:
+          ts = 0   
+          env.scheduler.distribute_policy.train()
       
       if eps%SYNC==0:
         env.scheduler.distribute_policy.update_target_model()
         env.scheduler.distribute_policy.save_model(MODEL_PATH)
-        
+      
+      if env.scheduler.distribute_policy.loss is not None:
+        losses.append([eps, env.scheduler.distribute_policy.loss])
+        rets.append([eps, ret])
+    
+    save_fig_losses(losses)
+    save_fig_return(rets)
+    save_fig_reward(rewards)
+    
+    return copy.deepcopy(env.scheduler.dump_schedules(NODE_DISTANCES))
 
 if __name__ == "__main__":
   train = Learning()
-  train.main()
+  _ = train.main()
